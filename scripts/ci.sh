65d8bcaed6eecd051f2a0886ed2c7ad1b3af19aa
Christopher Baklid <christopherb@unity3d.com>
2018-05-31 12:53:37 +0200

    use the virtualenv python binary in travis

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -3,5 +3,5 @@ echo $path
 chmod +x binaries/nsjail2.6-ubuntu-x86_64
 sudo updatedb
 sudo locate libprotobuf
-echo $(pwd)/binaries/nsjail2.6-ubuntu-x86_64 -Mo --rlimit_as 700 --chroot / -E LANG=en_US.UTF-8 -R/usr -R/lib -R/lib64 --user nobody --group nogroup --time_limit 2 --disable_proc --iface_no_lo --quiet -- /usr/bin/python3.6 -ISq -c 'print("test")'
-$(pwd)/binaries/nsjail2.6-ubuntu-x86_64 -Mo --rlimit_as 700 --chroot / -E LANG=en_US.UTF-8 -R/usr -R/lib -R/lib64 --user nobody --group nogroup --time_limit 2 --disable_proc --iface_no_lo --quiet -- /usr/bin/python3.6 -ISq -c 'print("test")'
+echo $(pwd)/binaries/nsjail2.6-ubuntu-x86_64 -Mo --rlimit_as 700 --chroot / -E LANG=en_US.UTF-8 -R/usr -R/lib -R/lib64 --user nobody --group nogroup --time_limit 2 --disable_proc --iface_no_lo --quiet -- $PYTHONEXECUTABLE -ISq -c 'print("test")'
+$(pwd)/binaries/nsjail2.6-ubuntu-x86_64 -Mo --rlimit_as 700 --chroot / -E LANG=en_US.UTF-8 -R/usr -R/lib -R/lib64 --user nobody --group nogroup --time_limit 2 --disable_proc --iface_no_lo --quiet -- $PYTHONEXECUTABLE -ISq -c 'print("test")'
--- tests/test_snekbox.py
+++ tests/test_snekbox.py
@@ -3,9 +3,9 @@ import pytest
 import os
 
 from snekbox import Snekbox
-
+python_binary = os.environ.get('PYTHONEXECUTABLE', '/usr/bin/python3.6')
 nsjail = os.sep.join([os.getcwd(), f'binaries{os.sep}nsjail2.6-ubuntu-x86_64'])
-snek = Snekbox(nsjail_binary=nsjail, python_binary='/usr/bin/python3.6')
+snek = Snekbox(nsjail_binary=nsjail, python_binary=python_binary)
 
 class SnekTests(unittest.TestCase):
 	def test_nsjail(self):
