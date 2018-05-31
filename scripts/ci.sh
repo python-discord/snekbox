137307ef3dece4324f4c3b7c6dc410a3da511f32
Christopher Baklid <christopherb@unity3d.com>
2018-05-31 11:26:26 +0200

    accept flag for apt-get

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 chmod +x binaries/nsjail2.6-ubuntu-x86_64
-apt-get update
-apt-get install libprotobuf-dev
+sudo apt-get -qq update
+sudo apt-get install -y libprotobuf-dev
 echo $(pwd)/binaries/nsjail2.6-ubuntu-x86_64 -Mo --rlimit_as 700 --chroot / -E LANG=en_US.UTF-8 -R/usr -R/lib -R/lib64 --user nobody --group nogroup --time_limit 2 --disable_proc --iface_no_lo --quiet -- /usr/bin/python3.6 -ISq -c 'print("test")'
 $(pwd)/binaries/nsjail2.6-ubuntu-x86_64 -Mo --rlimit_as 700 --chroot / -E LANG=en_US.UTF-8 -R/usr -R/lib -R/lib64 --user nobody --group nogroup --time_limit 2 --disable_proc --iface_no_lo --quiet -- /usr/bin/python3.6 -ISq -c 'print("test")'
