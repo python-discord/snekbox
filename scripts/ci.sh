8c82f85d767136710d148761fa9dc5c3f1fb6439
Christopher Baklid <christopherb@unity3d.com>
2018-05-31 12:35:53 +0200

    follow travis preinstall conventions

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 echo $path
 chmod +x binaries/nsjail2.6-ubuntu-x86_64
-sudo apt-get -qq update
-sudo apt-get install -y libprotobuf-dev
-sudo apt-get install -y libstdc++6
 sudo updatedb
 sudo locate libprotobuf
 echo $(pwd)/binaries/nsjail2.6-ubuntu-x86_64 -Mo --rlimit_as 700 --chroot / -E LANG=en_US.UTF-8 -R/usr -R/lib -R/lib64 --user nobody --group nogroup --time_limit 2 --disable_proc --iface_no_lo --quiet -- /usr/bin/python3.6 -ISq -c 'print("test")'
