a9528a58bf27a944f308b61fb8ba1eb4014703a6
Christopher Baklid <christopherb@unity3d.com>
2018-06-05 23:12:59 +0200

    trying an alias

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ import traceback
 
 
 def autodiscover():
-    container_names = ["rmq", "pdrmq", "snekbox_pdrmq_1", "pdrmq-ci"]
+    container_names = ["rmq", "pdrmq", "snekbox_pdrmq_1"]
 
     client = docker.from_env()
     for name in container_names:
