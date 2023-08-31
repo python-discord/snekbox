b0180ee7cded3ca51ab832892ae162f13a0e1783
Chris Lovering <chris.lovering.95@gmail.com>
2023-08-31 10:06:47 +0100

    squashme: add set -euo pipefail as sane defaults
    This also requires running under bash rather than sh

--- scripts/install_eval_deps.sh
+++ scripts/install_eval_deps.sh
@@ -1,3 +1,5 @@
+set -euo pipefail
+
 export PYTHONUSERBASE=/snekbox/user_base
 find /lang/python -mindepth 1 -maxdepth 1 -type d -exec \
     {}/bin/python -m pip install --user -U -r requirements/eval-deps.pip \;
